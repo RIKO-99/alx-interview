06161ef3d64afef237ceb39c5ab65723667a7529
RIKO-99 <rikohssn@gmail.com>
2023-10-19 19:50:06 -0700

    logparse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,50 @@
 #!/usr/bin/python3
-"""Script that reads stdin
-   line by line and computes metrics."""
+"""Write a script that reads stdin line by line and computes metrics
+"""
+
 
 import sys
 
+# store the count of all status codes in a dictionary
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
-def log_parse():
-    """Function that reads stdin line by line and computes metrics.
+total_size = 0
+count = 0  # keep count of the number lines counted
 
-    Args:
-        None
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
 
-    Returns:
-        None
-    """
-    status_dict = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0}
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-    total_size = 0
+            # check if the status code receive exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
 
-    while True:
-        for line in sys.stdin:
-            line = line.strip()
-            line = line.split()
+            # update total size
+            total_size += file_size
 
-            if len(line) > 2:
-                try:
-                    status = line[-2]
-                    size = line[-1]
+            # update count of lines
+            count += 1
 
-                    for k, v in status_dict.items():
-                        if status == k:
-                            status_dict[k] += 1
-                            total_size += int(size)
-                    print('File size: {}'.format(total_size))
-                    for key in sorted(status_dict):
-                        print('{}: {}'.format(key, status_dict[key]))
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
 
-                except KeyboardInterrupt:
-                    print('File size: {}'.format(total_size))
-                    for key in sorted(status_dict):
-                        print('{}: {}'.format(key, status_dict[key]))
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
+except Exception as err:
+    pass
 
-if __name__ == '__main__':
-    log_parse()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
