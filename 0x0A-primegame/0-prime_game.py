1e3fbe50feb043e1e4ce848393a64b138023b178
RIKO-99 <rikohssn@gmail.com>
2023-12-08 02:49:49 +0000

    is prime function

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,54 +1,53 @@
 #!/usr/bin/python3
 """The Game Module prime game challange."""
 
-
-def is_winner(x, nums):
+def iswinner(x, nums):
     """
-    is_winner: A function that determines if a player is the winner.
+    is_winner: A function that determines who wins the most rounds in the game.
 
     Args:
         x (int): The number of rounds.
-        nums (list): A list of n.
+        nums (list): A list of n for each round.
 
     Returns:
         str: The name of the player that won the most rounds.
-            If the winner cannot be determined, return None.
+             If the winner cannot be determined, return None.
     """
-    def filter_nums(n):
-        """filter_nums: A function that filters the list of n."""
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-
-        return [i for i in range(2, n + 1) if primes[i]]
-
-    def game(n):
-        """
-        game: A function that determines if a player is the winner.
-        """
-        primes = filter_nums(n)
-        nums_set = set(range(1, n + 1))
-        while nums_set and primes:
-            for p in primes:
-                multiples = set(range(p, n + 1, p))
-                nums_set -= multiples
-                if not nums_set:
-                    return "Maria"
-                primes = [q for q in primes if q not in multiples]
-                if not primes:
-                    return "Ben"
+    def is_prime(num):
+        """is_prime: Check if a number is prime."""
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
     maria_wins = 0
     ben_wins = 0
-    for i in range(x):
-        winner = game(nums[i])
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
+
+    for n in nums:
+        primes = [num for num in range(2, n + 1) if is_prime(num)]
+        nums_set = set(range(1, n + 1))
+        maria_turn = True
+
+        while nums_set and primes:
+            if maria_turn:
+                choice = min(primes)
+            else:
+                choice = max(primes)
+
+            multiples = set(range(choice, n + 1, choice))
+            nums_set -= multiples
+
+            if not nums_set:
+                if maria_turn:
+                    ben_wins += 1
+                else:
+                    maria_wins += 1
+                break
+
+            primes = [q for q in primes if q not in multiples]
+            maria_turn = not maria_turn
 
     if maria_wins > ben_wins:
         return "Maria"
